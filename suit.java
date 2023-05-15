af9dec574d44a4c5f5a32cc192e619bb4c8dac44
nguyen huu khang <nguyenkhang242333@gmail.com>
2023-05-16 00:49:33 +0700

    suit changes 16/05

--- suit.java
+++ suit.java
@@ -1,3 +1,15 @@
-public class suit {
-    
+public enum suit {
+    CLUB("Clubs"),
+    DIAMOND("Diamonds"),
+    HEART("Hearts"),
+    SPADE("Spades"),
+
+
+    String suitName;
+    suit(String suitName){
+        this.suitName = suitName;
+    }
+    public String toString(){
+        return suitName;
+    }
 }
