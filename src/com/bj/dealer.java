0d2af8f25318fa57c759f1adc3e1cd58a8e2af3f
maiquocbinh13 <binhaxdoc3@gmail.com>
2023-05-22 13:00:25 +0700

    repair dealer class

--- src/com/bj/dealer.java
+++ src/com/bj/dealer.java
@@ -1,12 +1,24 @@
+public class Dealer extends Person{
 
-public class Dealer extends man {
+    /**
+     * Create a new Dealer
+     */
     public Dealer(){
+
+        //Name the dealer "Dealer"
         super.setName("Dealer");
-    }
-    public void printFirstHand(){
-        System.out.println("The dealer's hands look like this:");
-        System.out.println(super.getHand().getCard(0));
-        System.out.println("The second card is face down");
 
     }
+
+    /**
+     * Prints the dealer's first hand, with one card face down.
+     */
+    public void printFirstHand(){
+        System.out.println("The dealer's hand looks like this:");
+        System.out.println(super.getHand().getCard(0));
+        System.out.println("The second card is face down.");
+    }
+
+
+
 }
