14a2dcd21bcc7297eb6f363f900bee60af28c124
maiquocbinh13 <binhaxdoc3@gmail.com>
2023-05-22 13:06:58 +0700

    launchpage class

--- src/com/bj/LaunchPage.java
+++ src/com/bj/LaunchPage.java
@@ -1,2 +1,32 @@
-package src.com.bj;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+
+public class LaunchPage {
+	JFrame frame = new JFrame();
+	
+	ImageIcon logo = new ImageIcon(".//res//logo.jpg");
+	LaunchPage(){
+		//Create game window
+        JFrame frame = new JFrame("BlackJack mien TAY");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        
+
+        //Set its size
+        Game blackjack = new Game();
+        frame.setSize(800, 600);
+
+        //Make a new blackjack Game object/JPanel
+        
+
+        //Add it to the frame and make it visible
+        frame.add(blackjack);
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        frame.setIconImage(logo.getImage());
+        
+	}
+}
 
