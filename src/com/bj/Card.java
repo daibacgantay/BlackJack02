854dd6d7d534cc3cee7269f65d25dd238c044d7f
daibacgantay <131435641+daibacgantay@users.noreply.github.com>
2023-05-25 21:40:16 +0700

    Update Card.java

--- src/com/bj/Card.java
+++ src/com/bj/Card.java
@@ -1,74 +1,72 @@
 package src.com.bj;
 
-public class Card implements Comparable<Card>{
+//  Tạo là bài có Rank và Suit chứa các method lấy giá trị và in ra lá bài
+ // implement Comparable để dùng method compare to sắp xếp lá bài theo thứ tự
+public class Card implements Comparable<Card> {
 
-    private Suit suit;
-    private Rank rank;
+     private Suit suit;
+     private Rank rank;
 
-   /**
-    *
-    * @param suit  The Suit of the card to be created
-    * @param rank  The Rank of the card to be created
-    */
-   public Card(Suit suit, Rank rank){
-       this.suit = suit;
-       this.rank = rank;
-   }
+    
+    
+      //tao rank va suit
+    
+    public Card(Suit suit, Rank rank){
+        this.suit = suit;
+        this.rank = rank;
+    }
 
-   /**
-    * Copy constructor
-    * @param card the card being copied
-    */
-   public Card(Card card){
-       this.suit = card.getSuit();
-       this.rank = card.getRank();
-   }
+    
+      //Tạo copy construtors
+     
+     
+    public Card(Card card){
+        this.suit = card.getSuit();
+        this.rank = card.getRank();
+    }
 
-   /**
-    *
-    * @return  The numerical value of the Card
-    */
-   public int getValue(){
-       return rank.rankValue;
-   }
+    
+     
+     //getter and setter
+     
+     
+    public int getValue(){
+        return rank.rankValue;
+    }
 
-   /**
-    *
-    * @return The suit of the Card.
-    */
-   public Suit getSuit(){
-       return suit;
-   }
+   
+    public Suit getSuit(){
+        return suit;
+    }
 
-   public Rank getRank(){
-       return rank;
-   }
+    public Rank getRank(){
+        return rank;
+    }
 
-   /**
-    *
-    * @return The Card as a readable string
-    */
-   public String toString(){
-       return ("["+rank+" of "+ suit + "] ("+this.getValue()+")");
+    
+    
+     
+     //in ra lá bài
+    public String toString(){
+        return ("["+rank+" of "+ suit + "] ("+this.getValue()+")");
 
-   }
+    }
 
-   /**
-    * Compare a card to another card, returning 1 if this is higher, -1 if this is lower, in case we want to sort cards
-    * @param c
-    * @return
-    */
-   @Override
-   public int compareTo(Card c) {
-       //if this card is greater than the other card
-       if(this.getValue() > c.getValue()){
-           return 1;
-       }
-       else if(this.getValue() < c.getValue()){
-           return -1;
-       }
-       else{
-           return 0;
-       }
-   }
+    
+     
+ 
+     //sắp xếp theo dộ lớn của lá bài 
+    @Override
+    public int compareTo(Card c) {
+        
+        if(this.getValue() > c.getValue()){
+            return 1;
+        }
+        else if(this.getValue() < c.getValue()){
+            return -1;
+        }
+        else{
+            return 0;
+        }
+    }
 }
